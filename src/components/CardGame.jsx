163c82a1ebd9dbbe9288e37359927f4986694ca3
Giuseppe Fanizzi <giuseppe.fanizzi.15@gmail.com>
2025-05-16 10:50:56 +0200

    modificato anchor in link

--- src/components/CardGame.jsx
+++ src/components/CardGame.jsx
@@ -1,5 +1,5 @@
 import LazyLoadGameImage from "./LazyLoadGameImage";
-import {Link} from "react-router";
+import { Link } from "react-router";
 import './css/cardgame.css'
 
 
@@ -7,25 +7,27 @@ import './css/cardgame.css'
 export default function Card({ game }) {
     const genres = game.genres.map((genre) => genre.name).join(", ");
     return (
-        
+
 
         <div className=" game-card" key={game.id}>
-             <LazyLoadGameImage image={game.background_image} />
-             <div className="game-card-content">
+            <LazyLoadGameImage image={game.background_image} />
+            <div className="game-card-content">
 
-            <h2 className="game-title">{game.name}</h2>
-            <p>Rating: {game.rating}</p>
-            <p className="release-date-anim">Release Date: {game.released}</p>
-            <p>Genres: {genres}</p>
-            <div className="game-card-footer">
+                <h2 className="game-title">{game.name}</h2>
+                <p>Rating: {game.rating}</p>
+                <p className="release-date-anim">Release Date: {game.released}</p>
+                <p>Genres: {genres}</p>
+                <div className="game-card-footer">
 
-           <a href={`/games/${game.slug}/${game.id}`} 
-           className="btn-cyberpunk pulse">
-            visita il gioco
-            </a>
+                    <Link
+                        to={`/games/${game.slug}/${game.id}`}
+                        className="btn-cyberpunk pulse"
+                    >
+                        visita il gioco
+                    </Link>
+                </div>
             </div>
-             </div>
         </div>
-        
+
     )
 };
